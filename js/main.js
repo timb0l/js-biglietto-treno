eaa5f9afda650e8834d7215115326d8e5555d296
Jam Timbol <jam.timbol00@gmail.com>
2023-07-03 17:29:32 +0200

    90%

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function submitForm(event){
+    event.preventDefault();
+    console.log("Passenger Details");
+
+    let userFullName = document.getElementById("userFullName").value;
+    let userKm = parseInt(document.getElementById("userKm").value);
+    let selectAge = parseInt(document.getElementById("underage").value);;
+
+    //ticket type
+        //???????????????
+    //wagon
+        let wagon = math.floor(math.random() * 10) + 1;
+        console.log(wagon)
+    //CpCode
+         let randomCode = math.floor(math.random() * 99999) + 1;
+            console.log(randomCode)
+    //ticketing
+
+        let ticketPrice = userKm * 0.21;
+            console.log(ticketPrice + "€")
+
+            if (underage){
+                let ticketPrice = ((userKm * 20)/100)
+            console.log(ticketPrice + "€")
+            }
+
+            else if (eldery){
+                let ticketPrice =((userKm * 40)/100)
+            console.log(ticketPrice + "€")
+            }
+
+
+
+document.getElementById("userFullName").innerHTML = containerName;
+document.getElementById("typeTicket").innerHTML = typeTicket;
+document.getElementById("wagon").innerHTML = wagon;
+document.getElementById("randomCode").innerHTML = randomCode;
+document.getElementById("ticketPrice").innerHTML = ticketPrice;
+
+
+
+
+}
